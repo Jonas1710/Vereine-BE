eef78d07404c665ad58e7c06ed3560ebbf5339c7
Mollet Jonas <bmollj@bbc-be.local>
2017-10-23 13:57:29 +0200

    Added verein_update.php

---
+++ view/verein_update.php
@@ -0,0 +1,46 @@
+<form class="form-horizontal" action="/verein/doCreate" method="post">
+	<div class="component" data-html="true">
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="name">Vereins Name</label>
+		  <div class="col-md-4">
+		  	<input id="name" name="name" type="text" placeholder="Vereins Name" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="kategorie">Kategorie</label>
+		  <div class="col-md-4">
+		  	<input id="kategorie" name="kategorie" type="text" placeholder="Kategorie" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="mitgliederanzahl">Anzahl Mitglieder</label>
+		  <div class="col-md-4">
+		  	<input id="mitgliederanzahl" name="mitgliederanzahl" type="number" placeholder="Anzahl Mitglieder" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="bild">Bild</label>
+		  <div class="col-md-4">
+		  	<input id="bild" name="bild" type="file" placeholder="bild" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="gründungsjahr">Gründungsjahr</label>
+		  <div class="col-md-4">
+		  	<input id="gründungsjahr" name="gründungsjahr" type="number" placeholder="Gründungsjahr" value="2000" class="form-control input-md">
+		  </div>
+		</div>
+		<div class="form-group">
+		  <label class="col-md-2 control-label" for="beschreibung">Beschreibung</label>
+		  <div class="col-md-4">
+		  	<textarea id="beschreibung" name="beschreibung" type="text" placeholder="Beschreibung" class="form-control input-md"></textarea>
+		  </div>
+		</div>
+		<div class="form-group">
+	      <label class="col-md-2 control-label" for="send">&nbsp;</label>
+		  <div class="col-md-4">
+		    <input id="send" name="send" type="submit" class="btn btn-primary">
+		  </div>
+		</div>
+	</div>
+</form>
